bd14493a6c4eea691708aa850947990847ef3778
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2014-07-15 16:56:28 +0100

    deploying to GH-pages

---
+++ js/myScript.js
@@ -0,0 +1,172 @@
+var myWeatherApp = {
+
+  init: function(){
+    var mapOptions = {
+      zoom: 12
+    };
+    myWeatherApp.config = {
+      map: new google.maps.Map(document.getElementById('map-canvas'), mapOptions),
+      geocoder: new google.maps.Geocoder(),
+      marker: 0
+    };
+
+    myWeatherApp.loadMapCurrentLoc();
+    myWeatherApp.loadWeather("Lagos");
+    myWeatherApp.setupListeners();
+  
+  },
+
+
+  loadMapCurrentLoc :  function () { 
+      // Try HTML5 geolocation
+      if(navigator.geolocation) 
+      {
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                                           position.coords.longitude);
+
+            myWeatherApp.config.marker = new google.maps.Marker({
+                map: myWeatherApp.config.map,
+                animation: google.maps.Animation.DROP,
+                position: pos
+            });
+
+            myWeatherApp.config.map.setCenter(pos);
+        },function() {
+          myWeatherApp.handleNoGeolocation(true);
+          });
+      
+      } 
+      else 
+      {
+        // Browser doesn't support Geolocation
+        myWeatherApp.handleNoGeolocation(false);
+      }
+  },
+
+  handleNoGeolocation:   function (errorFlag) {
+    if (errorFlag) 
+    {
+      var content = 'Error: The Geolocation service failed.';
+    } 
+    else 
+    {
+      var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+      map: myWeatherApp.config.map,
+      position: new google.maps.LatLng(60, 105),
+      content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    myWeatherApp.config.map.setCenter(options.position);
+  },
+
+  loadWeather:   function (location) {
+    $.simpleWeather({
+      location: location,
+      unit: 'f',
+      success: function(weather) {
+        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+        html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+        html += '<li class="currently">'+weather.currently+'</li>';
+        html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+
+        
+        if(weather.code>= 0 && weather.code<=17){
+        $("#weather").css("background-color","lightblue");
+        }
+        else if(weather.code> 17 && weather.code<=23){
+        $("#weather").css("background-color","lightblue");
+        }
+        else if(weather.code<23 ){
+        $("#weather").css("background-color","yellow");
+        }
+        else if(weather.code>= 24 && weather.code<=30){
+        $("#weather").css("background-color","lightblue");
+        }
+        else if(weather.code===31){
+        $("#weather").css("background-color","#grey");
+        }
+        else if(weather.code>32 && weather.code <36){
+        $("#weather").css("background-color","lightgrey");
+        }
+        else if(weather.code===32 && weather.code===36){
+        $("#weather").css("background-color","#FFCC66");
+        }
+        else if(weather.code>37){
+        $("#weather").css("background-color","lightblue");
+        }
+    
+        $("#weather").html(html);
+      },
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+    });
+  },
+
+  codeLocation: function(results, status){
+    if (status == google.maps.GeocoderStatus.OK) {
+      myWeatherApp.config.map.setCenter(results[0].geometry.location);
+      myWeatherApp.config.marker.setPosition(results[0].geometry.location);
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  },
+
+  placeMarker: function(location) {
+
+    if (myWeatherApp.config.marker == undefined){
+        myWeatherApp.config.marker.setPosition(location);
+    }
+    else{
+      myWeatherApp.config.marker.setPosition(location);
+
+    }
+    myWeatherApp.config.map.setCenter(location);
+    myWeatherApp.loadWeather(location.lat() + ',' + location.lng());
+
+  },
+
+  setupListeners: function(){
+    $('.js-geolocation').click(function() {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);//this stores
+        // the latitude and longitude for the present location 
+        myWeatherApp.loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+
+        myWeatherApp.config.marker.setPosition(latLng); 
+        myWeatherApp.config.map.setCenter(latLng);
+      });
+    });
+
+    $("#searchForm").submit(function(event){
+      event.preventDefault();
+      var address = $("#search").val();
+      myWeatherApp.loadWeather(address);
+      myWeatherApp.config.geocoder.geocode({ 'address': address}, myWeatherApp.codeLocation);
+       var address= $('#search').val("");
+    });
+
+    $('#search').keyup(function(event) {
+      if (event.keyCode == 13) {
+        event.preventDefault();
+      $('#searchForm').submit();
+      }
+    });
+
+    google.maps.event.addListener(myWeatherApp.config.map, 'click', function(event) {
+      myWeatherApp.placeMarker(event.latLng);
+    });
+
+  }
+
+};
+
+$(document).ready(function(){
+  myWeatherApp.init();  
+});
